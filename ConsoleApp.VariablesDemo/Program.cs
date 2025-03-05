60a33af8036b36e5dc0f9fcf7014481424d42e90
Nehemiah Brown <nbrowndeveloper@gmail.com>
2025-03-05 12:29:12 -0600

    Add project files.

---
+++ ConsoleApp.VariablesDemo/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string name = "Nehemiah Brown";
+
+Console.WriteLine(name);
+Console.WriteLine("I am " + name);
+Console.WriteLine($"They call me {name}");
+Console.WriteLine("I was given the name {0}", name);
+
+int age = 26;
---
+++ ConsoleApp.VariablesPractice/Program.cs
@@ -0,0 +1,19 @@
+﻿
+Console.WriteLine("Welcome to Symphony Bank");
+
+int userBalance = 0;
+int deposit = 0;
+int withdrawal = 0;
+
+Console.WriteLine("Enter your balance");
+userBalance = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter deposit amount");
+deposit = Convert.ToInt32(Console.ReadLine());
+userBalance = userBalance + deposit;
+Console.WriteLine($"The total in your bank account is: {userBalance}");
+
+Console.WriteLine("Enter withdrawal amount");
+withdrawal = Convert.ToInt32(Console.ReadLine());
+userBalance = userBalance - withdrawal;
+Console.WriteLine($"The total in your bank account is: {userBalance}");
